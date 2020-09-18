e900ef1aec0d034af8e9356bb7718253ff0b690c
Nikita Fedoseev <n.w.f@mail.ru>
2020-09-18 08:11:04 +0300

    First changes

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+function openList(evt, listName) {
+
+    let i, cas, tablinks;
+
+    cas = document.getElementsByClassName("cases");
+    for (i = 0; i < cas.length; i++) {
+        cas[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(listName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+}
+
+openList(evt, sites);
